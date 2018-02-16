96f62b1d1d1caa7172580c3f5536e22cd43fdecd
davidangel <david@davidangel.net>
2018-02-16 17:45:37 -0500

    Add basic seeding.

--- database/factories/ModelFactories.php
+++ database/factories/ModelFactories.php
@@ -13,10 +13,20 @@ use Faker\Generator as Faker;
 |
 */
 
+$factory->define(App\Pet::class, function (Faker $faker) {
+   return [
+       'name' => $faker->firstName,
+       'avatar' => 'default.jpg',
+       'favorite_place' => $faker->longitude . ',' . $faker->latitude,
+       'date_of_birth' => $faker->dateTimeThisDecade
+   ];
+});
+
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'avatar' => 'default.jpg',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        if ($this->command->confirm('This will delete existing data from the database and create new seed data. Are you sure you want to continue?')) {
+            DB::table('users')->delete();
+            DB::table('pets')->delete();
+            DB::table('tags')->delete();
+            DB::table('taggables')->delete();
+
+            // create dev user
+            factory(App\User::class)->make([
+                'email' => 'dev@dev.com'
+            ])->save();
+
+            // create users
+            factory(App\User::class, 9)->create()->each(function ($user) {
+
+
+                // (maybe) create pets
+                $pets = [];
+                factory(App\Pet::class, rand(0, 3))->create()->each(function ($pet) {
+                    $pets[] = $pet;
+                });
+
+                if(!empty($pets)) {
+                    $user->pets()->save($pets);
+                }
+
+            });
+
+            $this->command->info('Successfully seeded database with sample users and pets. (login with dev@dev.com/secret)');
+        } else {
+            $this->command->line('db:seed command has been cancelled.');
+        }
     }
 }
