0ae232f20124a6c02a73963536aa12f26e09fee1
davidangel <david@davidangel.net>
2018-02-16 17:01:05 -0500

    Add HasTags trait to Pet model.

--- app/Pet.php
+++ app/Pet.php
@@ -3,13 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+use Spatie\Tags\Tag;
 
 class Pet extends Model
 {
+    use HasTags;
 
     public function human()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function addBreed($tag='')
+    {
+        $tagWithType = Tag::findOrCreate([$tag], 'breedTag');
+        $this->attachTag($tagWithType);
+        return $this;
+    }
+
+    public function removeBreed($tag='')
+    {
+        $tagWithType = Tag::findOrCreate([$tag], 'breedTag');
+        $this->detachTag($tagWithType);
+        return $this;
     }
 
 }
