bf9b5dfafcb03a3750a8a7a9b6f04c801899826f
davidangel <david@davidangel.net>
2018-02-16 19:58:21 -0500

    Update sample layouts.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,18 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h2>Your Pets</h2>
+                    <div class="d-flex justify-content-center" >
+                    @foreach($user->pets as $pet)
+                        <div class="card">
+                            <img class="card-img-top" src="/uploads/avatars/{{ $pet->avatar }}" alt="{{ $pet->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $pet->name }}</h5>
+                                <a href="#" class="btn btn-primary">Edit</a>
+                            </div>
+                        </div>
+                    @endforeach
+                    </div>
                 </div>
             </div>
         </div>
